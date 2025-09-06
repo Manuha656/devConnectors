668dc21aa5eae5e5f93114415cbcfd9c5e7d7e32
Manuha <enjamurimanuha@gmail.com>
2025-09-06 13:03:10 +0530

    Initial commit2

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express()
-
+const connectDB = require('./config/db')
 
 app.get('/', (req,res)=>{
     res.send('API Running')
 })
+
+//connect DataBase
+connectDB()
+
+//Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000
 
