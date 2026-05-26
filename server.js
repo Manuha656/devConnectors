b5b64e1d3c4e60bdcc664be90d5233895e2453f8
Manuha656 <enjamurimanuha@gmail.com>
2026-05-26 18:02:07 +0530

    added cors support

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const app = express()
 const connectDB = require('./config/db')
 
-app.get('/', (req,res)=>{
+// Connect Database
+connectDB()
+
+// CORS
+app.use(cors({
+    origin: 'https://devconnecto.netlify.app'
+}))
+
+// Init middleware
+app.use(express.json({ extended: false }))
+
+app.get('/', (req, res) => {
     res.send('API Running')
 })
 
-//connect DataBase
-connectDB()
-
-//Init middleware
-app.use(express.json({extended:false}))
-
-//Define routes
+// Define routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
@@ -21,4 +27,4 @@ app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,()=>console.log(`Server started in the port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started in the port ${PORT}`))
